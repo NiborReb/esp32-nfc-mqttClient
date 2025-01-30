e2eedfce1ab37a0a746bdf9761003e019662d7f8
NiborErb <107545982+NiborReb@users.noreply.github.com>
2025-01-30 13:06:43 +0100

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,13 @@
+#ifndef CREDENTIALS_H_
+#define CREDENTIALS_H_
+/* WiFi username and password */
+const char* ssid = "Nur-gegen-Seggs";
+const char* password = "walburga";
+
+/* MQTT credentials and connection */
+const char* mqttServer = "192.168.39.64";
+const int mqttPort = 1883;
+const char* mqttUser = "nfc-reader";
+const char* mqttPassword = "seggs";
+
+#endif
